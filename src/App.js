e03693074f0300c3f9f3c7f0d682891df3344c4f
korenrolle <korenrolle21@gmail.com>
2022-12-06 05:59:14 -0800

    readme updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Quotes from './Components/Quotes';
+import { Routes, Route } from 'react-router-dom';
+import Author from './Components/Author.js';
+import About from './Components/About.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>
+          <a href="/">Meliora</a>
+        </h1>
       </header>
-    </div>
+      <main>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/" element={<Author />} />
+          <Route path="/" element={<Quotes />} />
+        </Routes>
+      </main>
+    </>
   );
 }
-
 export default App;
