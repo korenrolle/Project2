3034b6a2abf654ebf7ee4c11b38e4c0704aef424
hniloy96 <hamimniloy96@gmail.com>
2022-12-07 13:10:25 -0500

    koren and niloy has been combined

---
+++ src/Pages/Game.js
@@ -0,0 +1,22 @@
+
+
+// const Game = (props) => {
+//   const arr = [
+//     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+//   ];
+
+//   function RandoCharacter() {
+//     let randomLength = arr.length;
+
+//     let randomArray = Math.floor(Math.random() * randomLength);
+//     return randomArray;
+//   }
+//   console.log(RandoCharacter());
+
+//   return (
+//     <div>
+//       <form></form>
+//     </div>
+//   );
+// };
+// export default Game;
