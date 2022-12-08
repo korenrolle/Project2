bf5bcfcdc661bedb0a47d02c8a8f0addaa743f45
korenrolle <korenrolle21@gmail.com>
2022-12-08 11:01:54 -0800

    minor fixes

--- src/Pages/Character.js
+++ src/Pages/Character.js
@@ -42,36 +42,37 @@ const Home = (props) => {
 
   return (
     <div className="game">
-      <div id="questions"></div>
       {/* <div id="questionRick">
         <img src={myGif} alt="my-gif" />
       </div> */}
       {character ? (
-        <div id="q1">
-          {<h2>"What's your name?"</h2>}
-          <input></input>
-          <div id="q2">
-            {<h2>'How long does it take you to drink a cup of water?'</h2>}
+        <div id="questions">
+          <div id="q1">
+            {<h2>"What's your name?"</h2>}
             <input></input>
-            <div id="q3">
-              {<h2>"What is 10 Morglorp, zenfied to the nearest Gelop?"</h2>}
+            <div id="q2">
+              {<h2>'How long does it take you to drink a cup of water?'</h2>}
               <input></input>
-              {}
-              <div id="q4">
-                <h2>
-                  'If you had to choose between golf or football...sludge?'
-                </h2>
+              <div id="q3">
+                {<h2>"What is 10 Morglorp, zenfied to the nearest Gelop?"</h2>}
                 <input></input>
-                <button id="questionBtn"> submit</button>
-                <div id="char">
-                  <img src={character.image} alt="quote" />
-                  <h2>Name:{character.name}</h2>
-                  <h2>Character was Created:{character.created}</h2>
-                  <h2>Character is a: {character.gender}</h2>
-                  <h2>{character.species}</h2>
-                  <h2>Character Classified as:{character.type}</h2>
-                  <h2>Character is still:{character.status}</h2>
-                  <h2>Location:{character.location.name}</h2>
+                {}
+                <div id="q4">
+                  <h2>
+                    'If you had to choose between golf or football...sludge?'
+                  </h2>
+                  <input></input>
+                  <button id="questionBtn"> submit</button>
+                  <div id="char">
+                    <img src={character.image} alt="quote" />
+                    <h2>Name:{character.name}</h2>
+                    <h2>Character was Created:{character.created}</h2>
+                    <h2>Character is a: {character.gender}</h2>
+                    <h2>{character.species}</h2>
+                    <h2>Character Classified as:{character.type}</h2>
+                    <h2>Character is still:{character.status}</h2>
+                    <h2>Location:{character.location.name}</h2>
+                  </div>
                 </div>
               </div>
             </div>
